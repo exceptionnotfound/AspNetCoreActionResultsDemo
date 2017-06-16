2dd9ec255d6b66d5eba86fab5cc818fa9f7e34ff
exceptionnotfound <mpjones428@gmail.com>
2017-06-16 08:13:43 -0700

    Finished Content demo.

---
+++ AspNetCoreActionResultsDemo/Controllers/ContentController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreActionResultsDemo.Controllers
+{
+    public class ContentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PartialViewResult()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public IActionResult JsonResult()
+        {
+            return Json(new { message = "This is a JSON result.", date = DateTime.Now });
+        }
+
+        [HttpGet]
+        public IActionResult ContentResult()
+        {
+            return Content("Here's the ContentResult message.");
+        }
+
+        [HttpGet]
+        public IActionResult ViewComponentResult()
+        {
+            return ViewComponent()
+        }
+    }
+}
